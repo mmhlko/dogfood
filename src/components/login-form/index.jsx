66067702b0524ae4633455c26ff36536ec618d9a
mmhlko <maximmhl@mail.ru>
2023-04-26 01:39:21 +0700

    Отзывы, Модальные окна и роутинг между ними

--- src/components/login-form/index.jsx
+++ src/components/login-form/index.jsx
@@ -43,6 +43,8 @@ function LoginForm({onSubmit, onNavigate, modal}) {
                 id='email'
                 type='text'
                 placeholder='email'
+                autocomplete="none"
+                
             />
             {errors?.email && <p className='errorMessage'>{errors.email.message}</p>}
             <FormInput 
